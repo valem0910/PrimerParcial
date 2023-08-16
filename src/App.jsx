01558dccf20ea8ab2567f74aa15780c69baf0299
Valeria Montañez <valeriamontanez0910@gmail.com>
2023-08-16 03:12:24 -0300

    Primer Parcial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  
+
+  return (
+    <div className='Add'>
+      <h1>Carga de Estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+   
+  )
+}
+
+export default App
